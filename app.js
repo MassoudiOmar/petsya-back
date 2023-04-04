12923c3f5fe9f4fa0be9e9fadc73b51f2e1dc1d0
omar massoudi <massoudiomar54321@gmail.com>
2023-04-04 13:10:51 +0100

    ad

--- app.js
+++ app.js
@@ -19,15 +19,11 @@ app.use(cors({ origin: "*" }));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json());
-<<<<<<< HEAD
 app.use(function(req, res, next) {
   console.log(req.method, req.url);
   next();
 });
-=======
-app.use(cors({ origin: "*" }));
 
->>>>>>> 12510741b5f18c414ee21da8fdcbfad9babcd6a9
 
 
 //use routes
@@ -53,13 +49,8 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-<<<<<<< HEAD
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
-=======
-let server = app.listen(PORT, function () {
-  console.log(`Server running on ${PORT}`);
->>>>>>> 12510741b5f18c414ee21da8fdcbfad9babcd6a9
 });
 
--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,15 +1,22 @@
-var mysql = require('mysql2');
-const dotenv = require('dotenv')
-dotenv.config()
+var mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
 
-var connection = mysql.createConnection({
-  host     : process.env.MYSQL_HOST,
-  user     : process.env.MYSQL_USER,
-  password : process.env.MYSQL_PASSWORD,
-  database : process.env.MYSQL_DATABASE
+var connection = mysql.createPool({
+  connectionLimit: process.env.CONNECTIONLIMIT || 100,
+  host: process.env.MYSQL_HOST || "containers-us-west-24.railway.app",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "4iIpFiFPGAgoGOuZCIh2",
+  database: process.env.MYSQL_DATABASE || "railway",
+  charset: "cp1256",
+  port: process.env.DB_PORT || 6324,
 });
 
-connection.connect((err, success) => {
-  err ? console.log("connection failed", err) : console.log("Database connected successfully")
-})
+connection.getConnection((err, success) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected");
+  }
+});
 module.exports = connection;
